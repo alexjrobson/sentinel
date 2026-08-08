c2ecead3428359c6104d40849b49f80bf7ad6934
alexjrobson <alex_j_robson@hotmail.com>
2026-08-08 12:29:05 +0800

    file reader version one

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Sentinel is alive")
+import os
+
+def list_files_recursive(path="."):
+    files_info = {}
+    for root, _, files in os.walk(path):
+        sub_dir = files_info
+        for part in root[len(path):].strip(os.sep).split(os.sep):
+            sub_dir = sub_dir.setdefault(part, {})
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            sub_dir[file_name] = {
+                "size": os.path.getsize(file_path)
+            }
+    return files_info
+
+# Running the function and printing the output
+print(list_files_recursive())
